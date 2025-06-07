05b0955865101562cd39dd7f320600f1b30a29a9
cmlee <bal.dongdongdong@gmail.com>
2025-06-07 14:29:47 +0900

    :bug: Fix deps

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -5,4 +5,17 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+      external: [],
+    },
+  },
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
 });
